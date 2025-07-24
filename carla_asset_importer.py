bcbcd1047f7a0603117e960940baacc5e1605bf6
Alex Richardson <william.a.richardson@vanderbilt.edu>
2025-07-24 15:30:36 -0500

    Creating static method for creating a single tile.

--- carla_asset_importer.py
+++ carla_asset_importer.py
@@ -33,22 +33,24 @@ class CarlaAssetImporter:
     def generateTerrainPath(self, x, y):
         return os.path.join(self.terrain_path, "meshes/", f"{int(x*1000)}_{int(y*1000)}.obj")
 
-    def generateTerrainTile(self, x, y):
-        number_of_points_on_side = int(self.tile_size / self.tile_point_interval)
-        mesh_folder = os.path.join(self.terrain_path, "meshes/")
-        os.makedirs(mesh_folder, exist_ok=True)
-        mesh_path = self.generateTerrainPath(x,y)
-        xs = np.linspace(x, x + self.tile_size, number_of_points_on_side)
-        ys = np.linspace(y, y + self.tile_size, number_of_points_on_side)
+    @staticmethod
+    def _generateTerrainTileMethod(mesh_path, map_data, dem_data, bounding_box):
+        min_x, min_y, max_x, max_y = bounding_box
+        tile_size = max_x - min_x
+        number_of_points_on_side = int(tile_size / self.tile_point_interval)
+
+        xs = np.linspace(min_x, max_x, number_of_points_on_side)
+        ys = np.linspace(min_y, max_y, number_of_points_on_side)
         xv, yv = np.meshgrid(xs, ys)
         zv = np.zeros_like(xv)
         for i in range(yv.shape[0]):
             for j in range(xv.shape[1]):
-                zv[i][j] = self.map_data.minHeightAtXYMeters(self.dem_data, (xv[i][j], yv[i][j]))
+                zv[i][j] = map_data.minHeightAtXYMeters(dem_data, (xv[i][j], yv[i][j]))
         xv = xv.flatten()
         yv = yv.flatten()
         zv = zv.flatten()
         vertices = np.stack([xv, yv, zv], axis=1)
+
         faces = []
         for i in range(number_of_points_on_side - 1):
             for j in range(number_of_points_on_side - 1):
@@ -60,9 +62,11 @@ class CarlaAssetImporter:
                 faces.append([i0, i2, i1])
                 faces.append([i1, i2, i3])
         faces = np.array(faces)
-        uv_x = xv / self.tile_size
-        uv_y = yv / self.tile_size
+
+        uv_x = xv / tile_size
+        uv_y = yv / tile_size
         uvs = np.stack([uv_x, uv_y], axis=1)
+
         mesh = trimesh.Trimesh(vertices=vertices, faces=faces, process=False)
         mesh.visual.uv = uvs
         # Remember, in Unreal, x becomes y, and y becomes x.
@@ -74,11 +78,24 @@ class CarlaAssetImporter:
         mesh.apply_transform(R)
         #mesh.compute_vertex_normals()
         mesh.export(mesh_path)
+
         mesh_metadata = {}
-        mesh_metadata["x"] = y
-        mesh_metadata["y"] = x
-        mesh_metadata["tile_size_x"] = self.tile_size
-        mesh_metadata["tile_size_y"] = self.tile_size
+        mesh_metadata["min_x"] = min_x
+        mesh_metadata["min_y"] = min_y
+        mesh_metadata["max_x"] = max_x
+        mesh_metadata["max_y"] = max_y
+        return mesh_metadata
+
+    def getTerrainTileBoundingBox(self, x, y):
+        return np.array([x, y, x + self.tile_size, y + self.tile_size])
+
+    def generateTerrainTile(self, x, y):
+        mesh_folder = os.path.join(self.terrain_path, "meshes/")
+        os.makedirs(mesh_folder, exist_ok=True)
+        tile_bounding_box = self.getTerrainTileBoundingBox(x, y)
+        dem_data = self.map_data.loadDEMsFromBoundingBoxMeters(tile_bounding_box)
+        mesh_path = self.generateTerrainPath(x,y)
+        mesh_metadata = self._generateTerrainTileMethod(mesh_path, self.map_data, dem_data, x, y)
         self.metadata["terrain"][mesh_path] = mesh_metadata
 
     def generateTerrain(self):
